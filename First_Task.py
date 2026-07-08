6a8e0eb71a11c691c31ce1ca08cb38f0db337cd1
maksi@Chomper <maksimozerov2007@yandex.ru>
2026-07-08 21:32:48 +0300

    Визуальная полировка кода

--- First_Task.py
+++ First_Task.py
@@ -7,16 +7,9 @@ xmin = -10
 xmax = 10
 count = 200
 x = np.linspace(xmin, xmax, count)
-y = -np.abs(
-    np.sin(x) *
-    np.cos(A) *
-    np.exp(np.abs(1 - np.sqrt(x**2 + A**2) / np.pi))
-)
+y = -np.abs(np.sin(x) * np.cos(A) * np.exp(np.abs(1 - np.sqrt(x**2 + A**2) / np.pi)))
 
-plt.figure(figsize=(10, 5))
 plt.plot(x, y)
-plt.title("График функции")
-plt.grid(True)
 plt.show()
 
 root = ET.Element("data")
