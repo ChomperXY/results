470e7c499f8224a7f1850ce17c911d6a1fa3157b
maksi@Chomper <maksimozerov2007@yandex.ru>
2026-07-08 16:20:26 +0300

    Сохранение в XML файл

--- First_Task.py
+++ First_Task.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
 
 A = 9.66459
 xmin = -10
@@ -11,8 +12,19 @@ y = -np.abs(
     np.cos(A) *
     np.exp(np.abs(1 - np.sqrt(x**2 + A**2) / np.pi))
 )
+
 plt.figure(figsize=(10, 5))
 plt.plot(x, y)
 plt.title("График функции")
 plt.grid(True)
 plt.show()
+
+root = ET.Element("data")
+xval = ET.SubElement(root, "xdata")
+yval = ET.SubElement(root, "ydata")
+for xi, in zip(x):
+    ET.SubElement(xval, "x").text = f"{xi:.4f}"
+for yi, in zip(y):
+    ET.SubElement(yval, "y").text = f"{yi:.4f}"
+tree = ET.ElementTree(root)
+tree.write("First_Task.xml", encoding="utf-8", xml_declaration=True)
