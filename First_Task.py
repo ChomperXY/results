460725ea655586d934585740c288f726cfd5a9d8
maksi@Chomper <maksimozerov2007@yandex.ru>
2026-07-08 14:26:03 +0300

    Рисование графика

--- First_Task.py
+++ First_Task.py
@@ -11,4 +11,8 @@ y = -np.abs(
     np.cos(A) *
     np.exp(np.abs(1 - np.sqrt(x**2 + A**2) / np.pi))
 )
-
+plt.figure(figsize=(10, 5))
+plt.plot(x, y)
+plt.title("График функции")
+plt.grid(True)
+plt.show()
